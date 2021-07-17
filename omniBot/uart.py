3d191b6780ac92be2f3eef31ebfc01b47af1bf7d
YY <lyy8593@gmail.com>
2021-07-17 19:48:16 +0800

    Finish UART unit test Send/Read

--- omniBot/uart.py
+++ omniBot/uart.py
@@ -1,15 +1,55 @@
+from numpy.core.defchararray import encode
+from numpy.core.fromnumeric import shape
 import serial
-import RPi.GPIO
+import time
+import numpy as np
+import RPi.GPIO as GPIO
 
 class uart:
-    def __init__(self, baudrate):
-        self.Baudrate = baudrate
-        self.ser = serial.Serial("/dev/ttyACM1", baudrate, timeout = 0.2)
+    def __init__(self, baud):
+        self.ser = serial.Serial(
+            port = '/dev/serial0',
+            baudrate = baud,
+            timeout = 1
+        )
+        self.buff = np.empty(shape=0, dtype=int)
+        self.msg_len = 0
     
-    def receive(self):
-        command = ser.read()
-        print(command)
+    def received(self):
+        if self.ser.in_waiting>0:
+            self.buff = np.append(self.buff, int(ord(self.ser.read())))
+            if self.buff.size < 4:
+                return
+            
+            self.msg_len = self.buff[3]
+
+            if len(self.buff) < self.msg_len:
+                return
+
+            print(self.buff[:self.msg_len])
+            self.buff[0] = 0xDF
+            self.ser.write(list(self.buff[:self.msg_len]))
+            self.buff = self.buff[self.msg_len:]
     
+def getCurrentTimeInMs():
+    return time.time()*1000
+
 if __name__ == "__main__":
     pi_com = uart(9600)
-    #pi_com
+    
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(16, GPIO.OUT)
+    GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.output(16, 0)
+    
+    last_tick = getCurrentTimeInMs()
+    delay_time = 50 #unit:ms
+    while(GPIO.input(18)):
+        new_tick = getCurrentTimeInMs()
+        pi_com.received()
+        #print(new_tick)
+        if (new_tick - last_tick) >= delay_time:
+            last_tick = new_tick
+            
+    GPIO.cleanup()
+    
