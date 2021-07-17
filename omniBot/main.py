b93729a22ce7ab04e19d7b1af639907fd6ff471e
YY <lyy8593@gmail.com>
2021-07-17 22:03:54 +0800

    Modify main.py

--- omniBot/main.py
+++ omniBot/main.py
@@ -1,1 +1,15 @@
-print("test")
+import time
+import RPi.GPIO as GPIO
+import threading
+from uart import uart
+
+com1 = uart(9600)
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(18, GPIO.IN, GPIO.PUD_UP)
+
+while(GPIO.input(18)):
+    com1.received()
+
+GPIO.cleanup()
